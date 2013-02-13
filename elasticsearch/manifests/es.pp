6d287f2ab21683170b3477c35a3efcbf7c371cf0
strud <stefanrudnitzki@googlemail.com>
2013-02-13 12:41:36 +0100

    updates es version

--- elasticsearch/manifests/es.pp
+++ elasticsearch/manifests/es.pp
@@ -58,7 +58,7 @@ file { '/etc/environment':
 }
 
 exec { 'wget':
-	command => 'wget -O /vagrant/elasticsearch.deb https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.20.1.deb',
+	command => 'wget -O /vagrant/elasticsearch.deb https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-0.20.4.deb',
 	path    => '/usr/local/bin/:/bin/:/usr/bin/',
 	creates => '/vagrant/elasticsearch.deb', 	
 	require => [ Package['java7-runtime-headless'], Exec['update'], File['/etc/environment'] ],
